ebad4cff2b04783442f30b692350c78a04cafe73
Doug Russell <paperworkbin@gmail.com>
2010-05-14 01:12:14 -0400

    adding license headers to files

--- KATG Big/Classes/UIColor+Additions.h
+++ KATG Big/Classes/UIColor+Additions.h
@@ -4,7 +4,19 @@
 //
 //  Created by Doug Russell on 5/11/10.
 //  Copyright 2010 Doug Russell. All rights reserved.
-//
+//  
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//  
 
 #import <UIKit/UIKit.h>
 
@@ -25,7 +37,7 @@ struct rgba_color {
 + (struct hsv_color)HSVfromRGB:(struct rgba_color)rgb;
 + (struct rgba_color)RGBAWithColor:(UIColor *)color;
 
-//Based on http://www.drobnik.com/touch/2009/10/manipulating-uicolors/
+//Based partially on http://www.drobnik.com/touch/2009/10/manipulating-uicolors/
 
 - (UIColor *)colorByDarkeningColor;
 - (UIColor *)colorByLighteningColor;
